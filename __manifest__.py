283ca14939cf6c910e2ba31243b76ebef30bdff8
Taha Abujrad <tabujrad@giga.ly>
2023-08-09 23:32:06 +0200

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "disable_debug",
+
+    'summary': """
+        Control The use of Odoo Debug Mode By Access Group""",
+
+    'description': """
+        This module is used to disable the odoo debug mode for all users except for those
+        having the Use Debug Mode group
+    """,
+
+    'author': "Taha Abujrad",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'application': True,
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/groups.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
---
+++ models/ir_http.py
@@ -0,0 +1,15 @@
+from odoo import models
+from odoo.http import request
+from ..shared.user import (
+    can_enter_debug_mode
+)
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _handle_debug(cls):
+        if can_enter_debug_mode(request.env.user):
+            return super(IrHttp, cls)._handle_debug()
+        request.session.debug = ''
